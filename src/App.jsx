f77c47001872109e29a0a206626243ecb2ca6653
John Doe <nadia643@gmail.com>
2019-12-06 20:19:26 +0000

    logo should be here

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './styles/app.css';
+import NavBar from './components/navbar';
+
+const App = () => <NavBar />;
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/navbar.css';
+
+const NavBar = () => (
+  <div className="NavBar">
+    <div className="imageContainer">
+      <img src="./images/surrealestate.png" alt="Logo should be here" height="200" width="200" />
+    </div>
+  </div>
+);
+
+export default NavBar;
